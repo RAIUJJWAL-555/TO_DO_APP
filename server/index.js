9f503260b7818ac8069934a9f99eac48f5e3643b
ujjwak rai <ur069119@gmail.com>
2025-10-28 19:45:10 +0530

    Fixedinbox

--- server/index.js
+++ server/index.js
@@ -11,10 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todoapp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect('mongodb://localhost:27017/todoapp')
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
